55332f52f3e4126464f9c2319a5e8b8ad37af06f
imac <imac@imacs-iMac.local>
2023-12-29 11:37:17 +1300

    changes

--- app/routes/test.ts
+++ app/routes/test.ts
@@ -1,5 +1,7 @@
 import { ActionFunctionArgs, json } from "~/runtime.server";
 
+export const config = { runtime: "edge" };
+
 export async function action({ request, params }: ActionFunctionArgs) {
   return json(
     {},
@@ -7,7 +9,7 @@ export async function action({ request, params }: ActionFunctionArgs) {
       status: 200,
       headers: {
         Location: "/?_data=routes/_base._index",
-        'Content-Location': "/?_data=routes/_base._index",
+        "Content-Location": "/?_data=routes/_base._index",
       },
     }
   );
