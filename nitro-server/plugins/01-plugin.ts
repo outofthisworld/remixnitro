462d3840c699a9977f845ff05a02dae3631f0323
Dale <dale@farpoint.co.nz>
2023-12-29 12:42:27 +1300

    changes

--- nitro-server/plugins/01-plugin.ts
+++
@@ -1,5 +0,0 @@
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook("render:response", (response, { event }) => {
-    setResponseHeader(event, "X-Nitro-Plugin", "1");
-  });
-});
