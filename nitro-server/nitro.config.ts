0b3217426da5e940a5750124a12ef7228ef030b7
Dale <dale@farpoint.co.nz>
2023-12-29 11:59:02 +1300

    changes

--- nitro-server/nitro.config.ts
+++ nitro-server/nitro.config.ts
@@ -2,7 +2,7 @@ import * as security from './config/security';
 
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  preset: "vercel",
+  preset: "vercel-edge",
   output: {
     dir: ".build/nitro-server-build",
     serverDir: ".build/nitro-server-build/server",
