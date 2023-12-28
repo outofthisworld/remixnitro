81ce0c04478b7c35dfe4e9c3adcecf0f1b37fc53
Dale <dale@farpoint.co.nz>
2023-12-29 12:31:11 +1300

    changes

--- nitro-server/routes/index.ts
+++ nitro-server/routes/index.ts
@@ -1,7 +1,14 @@
 import { toWebRequest, eventHandler } from "#imports";
-import * as build from "../.build/remix-server-build/index.mjs";
-import { ServerBuild, createRequestHandler } from "../../app/runtime.server";
+import { ServerBuild, createRequestHandler } from "@vercel/remix";
 
+/**
+ * Remix server build as entrypoint.
+ */
+import * as build from "../.build/remix-server-build/index.mjs";
+
+/**
+ * Serve remix.
+ */
 export default eventHandler(async (event) => {
   return createRequestHandler(build as unknown as ServerBuild)(
     toWebRequest(event),
